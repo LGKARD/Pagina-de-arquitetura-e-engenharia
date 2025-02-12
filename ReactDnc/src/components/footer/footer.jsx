5ddd8832af841a4df47704cc597822452ae6fa18
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-02-12 20:42:49 -0300

    Finalizando estilização do footer

--- ReactDnc/src/components/footer/footer.jsx
+++ ReactDnc/src/components/footer/footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
             <div className="container">
                 <div className="d-flex jc-between">
                     <div className="footer-logo-col">
-                        <img src={Logo} alt="" />
+                        <img src={Logo} className="footer-logo" />
                         <p className="grey-1-color">A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
                         <div className="d-flex social-links">
                             <a href="#" target="_blank">
@@ -32,10 +32,33 @@ function Footer() {
                                 <img src={InstagramIcon} />
                             </a>
                         </div>
+
                     </div>
-
+                    <div className="d-flex">
+                        <div className="footer-col">
+                            <h3>Pages</h3>
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/projects">Projects</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                            </ul>
+                        </div>
+                        <div className="footer-col">
+                            <h3>Contact</h3>
+                            <p className="grey-1-color">R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030 </p>
+                            <p className="grey-1-color">suporte@escoladnc.com.br</p>
+                            <p className="grey-1-color">(19) 99187-4342</p>
+                        </div>
+                    </div>
                 </div>
-
+                <div className="d-flex jc-between footer-copy">
+                    <p className="grey-1-color">Copyright © DNC - 2024</p>
+                    <div className="langs-area d-flex">
+                        <img src={BrasilIcon} height="29px" />
+                        <img src={UsaIcon} height="29px"/>
+                    </div>
+                </div>
             </div>
         </footer>
     )
