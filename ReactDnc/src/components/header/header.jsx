4c30758324f48bbf33e58faf851f0b7b200b5531
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-02-18 07:25:57 -0300

    Criação do menu mobile utilizando useState

--- ReactDnc/src/components/header/header.jsx
+++ ReactDnc/src/components/header/header.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 //ASSETS
 import './header.css'
 import Logo from '../../assets/dnc-logo 2.svg'
 
+//COMPONENTS
+import Button from "../button/button"
+
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => setIsOpen(!isOpen)
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-between">
                     <Link to="/"><img src={Logo} alt="" /></Link>
-                    <nav>
+                    <div className="mobile-menu">
+                        <Button buttonStyle="secondary" onClick={toggleMenu}>Menu</Button>
+
+                    </div>
+                    <nav className={`${isOpen ? 'open' : ''}`}>
+                        <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
+                            X
+                        </Button>
                         <ul className="d-flex">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/about">About</Link></li>
