bb1ea70636169fcdb1abe6d5263a72bab54e6a03
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-02-22 07:53:30 -0300

    Validando formulário na pagina contact

--- ReactDnc/src/components/contactForm/contactForm.jsx
+++ ReactDnc/src/components/contactForm/contactForm.jsx
@@ -1,25 +1,64 @@
+import { useEffect, useState } from 'react'
 import './contactForm.css'
+import '../button/button.css'
+
+// COMPONENTS
 import Button from '../button/button'
 
-
 function ContactForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+    const [isFormValid, setIsFormValid] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (isFormValid) {
+            alert("Formulário enviado com sucesso!")
+        }
+    }
+
+    useEffect(() => {
+        const isValidEmail = (email) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            return emailRegex.test(email)
+        }
+        const isValid = formData.name.trim() &&
+            formData.email.trim() &&
+            isValidEmail(formData.email) &&
+            formData.message.trim()
+
+        setIsFormValid(isValid)
+    }, [formData])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+
     return (
         <div className='contact-form d-flex fd-column al-center'>
             <h2>We love meeting new people and helping them.</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='d-flex form-group'>
                     <input
                         className='form-input'
                         type="text"
                         id='name'
                         name='name'
-                        placeholder="Name *" />
+                        placeholder="Name *"
+                        value={formData.name}
+                        onChange={handleChange} />
                     <input
                         className='form-input'
                         type="email"
                         id='email'
                         name='email'
-                        placeholder="Email *" />
+                        placeholder="Email *"
+                        value={formData.email}
+                        onChange={handleChange} />
                 </div>
                 <div className='d-flex form-group'>
                     <textarea
@@ -27,12 +66,14 @@ function ContactForm() {
                         id='message'
                         name='message'
                         placeholder="Message *"
+                        value={formData.message}
+                        onChange={handleChange}
                         rows="4">
                     </textarea>
                 </div>
 
                 <div className='al-center d-flex jc-end form-group'>
-                    <Button type='submit' buttonStyle="secondary">Enviar</Button>
+                    <Button type='submit' buttonStyle={`secondary ${!isFormValid ? 'disabled' : ''}`}>Enviar</Button>
                 </div>
 
             </form>
